1fad05d8d14c3ac932fc4aba6a3dbdd88eb2292e
benlusamba <benlusamba@gmail.com>
2020-08-30 15:36:56 -0400

    file support

--- fetch_bls.sh
+++ fetch_bls.sh
@@ -4,3 +4,10 @@ curl https://www.bls.gov/web/metro/ssamatab1.txt -o projects/gov_api/bls.json
 
 #Fetch data from BLS and dump to csv file
 curl https://www.bls.gov/web/metro/ssamatab1.txt -o projects/gov_api/bls.csv
+
+# Feth data from BLS and dump to txt file
+curl https://www.bls.gov/web/metro/ssamatab1.txt -o projects/gov_api/bls.txt
+
+## TO DO ##
+# Dyanmic support for multi-file extract
+# Compression Support
