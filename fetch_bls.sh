0479e67bd04efcfe08f770ef20bc0513f5d3d9f2
benlusamba <benlusamba@gmail.com>
2020-12-15 08:36:12 -0500

    bls test cases

--- fetch_bls.sh
+++ fetch_bls.sh
@@ -1,12 +1,12 @@
 #Fetch data from BLS and Dump to JSON file
 
-curl https://www.bls.gov/web/metro/ssamatab1.txt -o projects/gov_api/bls.json 
+curl https://www.bls.gov/web/metro/ssamatab1.txt -o gov_api/bls.json 
 
 #Fetch data from BLS and dump to csv file
-curl https://www.bls.gov/web/metro/ssamatab1.txt -o projects/gov_api/bls.csv
+curl https://www.bls.gov/web/metro/ssamatab1.txt -o gov_api/bls.csv
 
 # Feth data from BLS and dump to txt file
-curl https://www.bls.gov/web/metro/ssamatab1.txt -o projects/gov_api/bls.txt
+curl https://www.bls.gov/web/metro/ssamatab1.txt -o gov_api/bls.txt
 
 ## TO DO ##
 # Dyanmic support for multi-file extract
